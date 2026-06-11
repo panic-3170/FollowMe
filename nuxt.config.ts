2ec7bad32fd853560135917f06009c60979926c9
enfu <516954023@qq.com>
2026-06-11 21:45:01 +0800

    build: 移除动态 sitemap 生成逻辑，改用预生成静态 sitemap
    1. 从nuxt配置中移除sitemap.xml路由
    2. 删除服务端动态生成sitemap的工具和路由文件
    3. 添加预构建的静态sitemap.xml文件

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -30,7 +30,7 @@ export default defineNuxtConfig({
     preset: 'static',
     prerender: {
       crawlLinks: true,
-      routes: ['/', '/writing', '/404.html', '/sitemap.xml']
+      routes: ['/', '/writing', '/404.html']
     }
   }
 })
